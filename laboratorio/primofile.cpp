cd09c5c99a1df63b94399177ca7c3453540e641a
Fantomarc <marco.sergi@salveminialessano.edu.it>
2025-09-18 09:42:40 +0200

    Create primofile.cpp

---
+++ laboratorio/primofile.cpp
@@ -0,0 +1,8 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using nasmespace std;
+int main() {
+    string nomecognome ="Marco Sergi";
+    cout<<"ciao "+nomecognome;
+    return 0;
+}
