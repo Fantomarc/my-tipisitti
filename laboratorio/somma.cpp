91962a51015eaf91802e1faef1921b0f3ecc1235
Fantomarc <marco.sergi@salveminialessano.edu.it>
2025-09-25 12:33:38 +0200

    Create somma.cpp
    Ho creato la prima versione del file per la somma!

---
+++ laboratorio/somma.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b;
+    
+    cout<< "Inserisci primo valore per la somma: ";
+    cin >> a;
+    cout<< "Inserisci secondo valore per la somma: ";
+    cin >> b;
+    
+    //somma
+    
+    int somma= a + b;
+    
+    cout << "La somma dei due valori è: " << somma;
+    return 0;
+}
